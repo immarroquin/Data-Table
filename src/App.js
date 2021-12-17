7727c48e4107780129b9d6aeeda0064539c11da4
immarroquin <lorenamarroquin56@gmail.com>
2021-12-17 18:50:00 -0500

    Creacion de tabla con bootstrap que contiene el consumo de una api .json, realizado con react.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from "react";
+import "./App.css";
+import gravatar from "./Img/gravatar.png";
 
 function App() {
+  // console.log('aqui estoy');
+  const [data, setData] = useState([]);
+
+  const getProduct = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((users) => users.json())
+      .then((data) => setData(data));
+  };
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="container-table">
+        <table className="table table-striped table-dark">
+          <thead>
+            <tr>
+              <th scope="col">Imagen</th>
+              <th scope="col">Nombre</th>
+              <th scope="col">Correo</th>
+              <th scope="col">Ciudad</th>
+              <th scope="col">Compañia</th>
+            </tr>
+          </thead>
+          {data.map((user) => (
+            <tbody key={user.id}>
+              <tr>
+                <th scope="row">
+                  <img className="gravatar" src={gravatar} />
+                </th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.address.city}</td>
+                <td>{user.company.name}</td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      </div>
+    </Fragment>
   );
 }
 
